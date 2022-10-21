f48fee938d836e8a45bc9f476050475668a97a89
plotko.2287@gmail.com <plotko.2287@gmail.com>
2022-10-21 21:18:02 +0300

    .

---
+++ Public transport/Models/Relationship between transports and stops.cs
@@ -0,0 +1,9 @@
+﻿namespace Public_transport.Models
+{
+    public class Relationship_between_transports_and_stops
+    {
+        public int Id { get; set; }
+        public int IdTransport { get; set; }
+        public int IdStops { get; set; }
+    }
+}
