e0b13d1f5c4c69175b4afbe6c8c6ec3ba3bd5567
Антон Плотко <plotko.03@mail.ru>
2022-10-21 15:37:27 +0300

    Used data from the database to create Trolleybuses.cshtml and Buses.cshtml

--- Public transport/Pages/Buses.cshtml.cs
+++ Public transport/Pages/Buses.cshtml.cs
@@ -1,12 +1,27 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Public_transport.Models;
 
 namespace Public_transport.Pages
 {
     public class BusesModel : PageModel
     {
-        public void OnGet()
+        public PublicTransport[] ResBus = new PublicTransport[0];
+
+        public void OnGet(string? Type)
         {
+
+            using (Context db = new Context())
+            {
+                PublicTransport[] BusArray = db.Transports.ToArray();
+                ResBus = (from Bus in BusArray where (FilterTransport(Bus, Type ?? "")) select Bus).ToArray();
+            }
+        }
+
+        private bool FilterTransport(PublicTransport bus, string searchingBus)
+        {
+            bool isFinded = (bus.Type?.Contains(searchingBus) ?? false);
+            return isFinded;
         }
     }
 }
--- Public transport/Pages/Trolleybuses.cshtml.cs
+++ Public transport/Pages/Trolleybuses.cshtml.cs
@@ -1,12 +1,27 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Public_transport.Models;
 
 namespace Public_transport.Pages
 {
     public class TrolleybusesModel : PageModel
     {
-        public void OnGet()
+        public PublicTransport[] ResTroll = new PublicTransport[0];
+      
+        public void OnGet(string? Type)
         {
+            
+            using (Context db = new Context()) 
+            {
+                PublicTransport[]TrollArray=db.Transports.ToArray();
+                ResTroll = (from Trl in TrollArray where (FilterTransport(Trl, Type ?? ""))select Trl).ToArray();
+            }
+        }
+
+        private bool FilterTransport(PublicTransport trl, string searchingTroll) 
+        {
+            bool isFinded = (trl.Type?.Contains(searchingTroll) ?? false);
+            return isFinded;
         }
     }
 }
