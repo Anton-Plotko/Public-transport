e787635b3ffd4513c5294873b9b946ba80b6f144
Антон Плотко <plotko.03@mail.ru>
2022-10-24 16:06:42 +0300

    Added data to relationship database

--- Public transport/Models/Relationship between transports and stops.cs
+++ Public transport/Models/Relationship between transports and stops.cs
@@ -5,5 +5,7 @@
         public int Id { get; set; }
         public int IdTransport { get; set; }
         public int IdStops { get; set; }
+        public string TimeMonFri { get; set; }
+        public string TimeSaSu { get; set; }
     }
 }
