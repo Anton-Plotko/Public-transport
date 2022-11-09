db68652b9b3287b42932cbf62856739d86717c4e
plotko.2287@gmail.com <plotko.2287@gmail.com>
2022-11-09 09:33:48 +0300

    .

--- Public transport/Pages/Stops.cshtml.cs
+++ Public transport/Pages/Stops.cshtml.cs
@@ -1,12 +1,38 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Public_transport.Models;
+using System.Numerics;
+using static System.Formats.Asn1.AsnWriter;
 
 namespace Public_transport.Pages
 {
     public class StopsModel : PageModel
     {
-        public void OnGet()
+        public Relationship_between_transports_and_stops[] ResIdStops = new Relationship_between_transports_and_stops[0];
+        public Stop[] ResNameStops = new Stop[0];
+        public void OnGet(int IdTransport)
         {
+            using (Context db = new Context())
+            {
+                Relationship_between_transports_and_stops[] IdStopsArray = db.Relationships.ToArray();
+                ResIdStops = (from Stp in IdStopsArray where (FilterStops1(Stp, IdTransport)) select Stp).ToArray();
+                Stop[] StopArray = db.Stops.ToArray();
+                foreach(Relationship_between_transports_and_stops stops in ResIdStops)
+                {
+                    ResNameStops = (from Stops in StopArray where (FilterStops2(Stops,stops.IdStops)) select Stops).ToArray();
+                }
+            }
+        }
+
+        private bool FilterStops1(Relationship_between_transports_and_stops stp, int searchingStops)
+        {
+            bool isFinded = stp.IdTransport.Equals(searchingStops);
+            return isFinded;
+        }
+        private bool FilterStops2(Stop stop,int searchingStop)
+        {
+            bool isFinded = searchingStop.Equals(stop.Id);
+            return isFinded;
         }
     }
 }
