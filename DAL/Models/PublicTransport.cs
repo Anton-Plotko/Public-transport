249d75c6912209353b047e77c1aca75e7bb82978
Антон Плотко <plotko.03@mail.ru>
2022-11-14 16:45:23 +0300

    .

--- DAL/Models/PublicTransport.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Public_transport.Pages
-{
-    public class PublicTransport
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Type { get; set; }
-    }
-}
--- DAL/Models/Stop.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Public_transport.Models
-{
-    public class Stop
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-    }
-}
