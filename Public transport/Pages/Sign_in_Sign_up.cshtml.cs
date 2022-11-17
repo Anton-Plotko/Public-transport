461ea729bca347e9f043d940e17e3606f46585bc
Антон Плотко <plotko.03@mail.ru>
2022-11-17 15:04:42 +0300

    .

---
+++ Public transport/Pages/Sign_in_Sign_up.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Public_transport.Pages
+{
+    public class Sign_in_Sign_upModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
